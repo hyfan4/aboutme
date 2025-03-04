83784479ce3b01c01f5244e265138094df3b0ec9
hyfan4 <lisa100177@gmail.com>
2025-03-04 11:01:40 +0800

    version 8: PHP

---
+++ about.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>蕭雅帆簡介</title>
+	<style type="text/css">
+		* { font-family:"標楷體"; margin-left:auto; margin-right:auto;}
+		h1 {color:blue; font-size:60px;}
+		h2 {color:purple; font-size:40px;}
+	</style>
+	<script>
+		function change1() {
+  			document.getElementById("pic").src = "mountain.jpg";
+  			document.getElementById("h2text").innerText = "靜宜資管";
+		}
+
+		function change2() {
+  			document.getElementById("pic").src = "cliff.jpg";
+  			document.getElementById("h2text").innerText = "Tzyy-Ching Yang";
+		}
+	</script>
+
+
+</head>
+<body>
+	<table width="70%">
+		<tr>
+			<td><img src="cliff.jpg" width="110%" 
+				id="pic" onmouseover="change1()" onmouseout="change2()">
+				
+				</img>
+			</td>
+			<td>
+				<h1>蕭雅帆</h1>
+				<h2 id="h2text">Ya-Fan Hsiao</h2>
+			</td>
+		</tr>
+
+
+	</table>
+	
+	<table width="70%" border="1">
+
+		<tr>
+			<td>
+				其他網頁：<a href="https://www.youtube.com/watch?v=wBY5g_UeDmQ">https://www.youtube.com/watch?v=wBY5g_UeDmQ</a><br>
+				IG：<a href="https://www.instagram.com/y.fannnn/">https://www.instagram.com/y.fannnn/</a><br>
+				Tel: <a href="tel:0965324601">0965324601</a><br>
+				E-Mail: <a href="mailto:lisa100177@gmail.com">lisa100177@gmail.com</a><br></img>
+			</td>
+
+			<td>
+				大象席地而坐電影配樂<br>
+				<audio controls>
+					<source src="elephant.mp3" type="audio/mP3">
+				</audio><br>
+
+			</td>
+
+			<td>
+				不要去臺灣<br>
+				<iframe src="https://www.youtube.com/embed/pW88QFpHXa8" allowfullscreen></iframe></img></td>
+		</tr>
+
+	</table>
+
+	<table>
+		<tr>
+			<td>
+				<iframe
+    				allow="microphone;"
+    				width="350"
+    				height="430"
+    				src="https://console.dialogflow.com/api-client/demo/embedded/6acbf2e4-1788-41af-8ff4-297748699185">
+				</iframe>
+			</td>
+		</tr>
+	</table>
+
+	<?php echo date("Y-m-d") ?>
+
+</body>
+</html>
